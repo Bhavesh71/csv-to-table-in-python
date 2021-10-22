41fc6a8477b37127bd706fab92cd284a7daf6dea
Bhavesh_71 <77270386+Bhavesh71@users.noreply.github.com>
2021-10-22 20:43:58 +0530

    Code update

--- csv_to_table.py
+++ csv_to_table.py
@@ -1,9 +1,9 @@
-#https://pypi.org/project/prettytable/ (module documentation)
-#pip install prettytable(install module from cmd or powershell)
+# https://pypi.org/project/prettytable/ (module documentation)
+# pip install prettytable (install module from cmd or powershell)
 from prettytable import from_csv
 print('''         CSV TO TABLE CONVERTER      ''')
-path = input(str("Enter Path of CSV file (without quotes) :"))#importing path of csv file
-csv_file = open(path)#opening csv file
-csv_data = from_csv(csv_file) #conerting csv into tabular form 
-print(csv_data) #printing tabular form of csv file
+path = input(str("Enter Path of CSV file (without quotes) : ")) # importing path of csv file
+CSV_file = open(path) # opening csv file
+CSV_data = from_csv(CSV_file) # conerting csv into tabular form 
+print(CSV_data) # printing tabular form of csv file
 
